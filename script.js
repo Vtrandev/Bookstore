d060886db7996cd0e3d1f7b15eedb9a52068a663
Vuong Tran <121814098+Vtrandev@users.noreply.github.com>
2024-02-13 18:04:33 -0800

    Book removal function added by button which filters array

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
-const myLibrary = [];
+let myLibrary = [];
 let htmlString = "";
+let count = 0;
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, id) {
   // the constructor...
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.id = id;
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
   };
 }
 
-function showBooks(book) {
+function showBooks() {
   htmlString = "";
   myLibrary.forEach(
     (e) =>
       (htmlString += `<div class="book__card">
-                <h2>Title: ${e.title}</h2>
-                <p>Author: ${e.author}</p>
-                <p>Pages: ${e.pages}</p>
-                <p>Read: ${e.read}</p>
+                <h2 class="card__item">Title: ${e.title}</h2>
+                <p class="card__item">Author: ${e.author}</p>
+                <p class="card__item">Pages: ${e.pages}</p>
+                <p class="card__item">Read: ${e.read}</p>
+                <p class="card__item">ID: ${e.id}</p>
+                <button onclick="removeBooks(${e.id})">Remove</button>
               </div>`)
   );
-  return htmlString;
+  
+  bookShowCase.innerHTML = htmlString;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function removeBooks(bookId) {
+  console.log(`Removed book: ${bookId}`)
+  myLibrary = myLibrary.filter(book => book.id !== bookId)
+  console.log(myLibrary);
+  showBooks();
+}
+
+function addBookToLibrary(title, author, pages, read, id) {
   // do stuff here
-  const newBook = new Book(title, author, pages, read);
+  const newBook = new Book(title, author, pages, read, id);
   myLibrary.push(newBook);
-  showBooks(myLibrary);
-  bookShowCase.innerHTML = htmlString;
+  showBooks();
   // myLibrary.map((e) => showBooks(e)).join("");
 }
 
@@ -41,7 +52,8 @@ function getBooksFromUser(e) {
   author = e.target[2].value;
   pages = e.target[3].value;
   read = e.target[4].value;
-  addBookToLibrary(title, author, pages, read);
+  id = count++
+  addBookToLibrary(title, author, pages, read, id);
 }
 
 const btnAddBook = document.querySelector(".form");
